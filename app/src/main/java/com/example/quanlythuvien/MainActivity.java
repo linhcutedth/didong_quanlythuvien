ac8f000aab58ab4cd049c1a63440b0ddeeeab8e8
honghai19521467 <19521467@gm.uit.edu.vn>
2022-05-02 22:14:22 +0700

    menu_nav
    left_menu_navigation

--- app/src/main/java/com/example/quanlythuvien/MainActivity.java
+++ app/src/main/java/com/example/quanlythuvien/MainActivity.java
@@ -1,14 +1,84 @@
 package com.example.quanlythuvien;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.quanlythuvien.fragment.AccountFragment;
+import com.example.quanlythuvien.fragment.BookFragment;
+import com.example.quanlythuvien.fragment.HomeFragment;
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private static final int FRAGMENT_HOME = 0;
+    private static final int FRAGMENT_BOOK = 1;
+    private static final int FRAGMENT_ACCOUNT = 2;
+
+    private  int mCurrentFragment = FRAGMENT_HOME;
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,mDrawerLayout,toolbar,
+                R.string.nav_drawer_open, R.string.nav_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        replaceFragment(new HomeFragment());
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.nav_home){
+            if(mCurrentFragment != FRAGMENT_HOME){
+                replaceFragment(new HomeFragment());
+                mCurrentFragment = FRAGMENT_HOME;
+            }
+        }else if (id == R.id.nav_book){
+            if(mCurrentFragment != FRAGMENT_BOOK){
+                replaceFragment(new BookFragment());
+                mCurrentFragment = FRAGMENT_BOOK;
+            }
+        }else if (id == R.id.nav_account){
+            if(mCurrentFragment != FRAGMENT_ACCOUNT){
+                replaceFragment(new AccountFragment());
+                mCurrentFragment = FRAGMENT_ACCOUNT;
+            }
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_frame,fragment);
+        transaction.commit();
     }
 }
