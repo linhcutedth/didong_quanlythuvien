65adb9afde00910dbfa118d31fd3d4f27f4b7b67
honghai19521467 <19521467@gm.uit.edu.vn>
2022-05-04 13:30:57 +0700

    nav_bottom
    nav_bottom

--- app/src/main/java/com/example/quanlythuvien/HomeActivity.java
+++ app/src/main/java/com/example/quanlythuvien/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.quanlythuvien;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -10,11 +11,16 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.quanlythuvien.fragment.AccountFragment;
 import com.example.quanlythuvien.fragment.BookFragment;
 import com.example.quanlythuvien.fragment.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -24,6 +30,8 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     private  int mCurrentFragment = FRAGMENT_HOME;
     private DrawerLayout mDrawerLayout;
+    private BottomNavigationView mBottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +39,8 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mBottomNavigationView = findViewById(R.id.nav_bottom);
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,mDrawerLayout,toolbar,
@@ -42,21 +52,30 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         navigationView.setNavigationItemSelectedListener(this);
 
         replaceFragment(new HomeFragment());
+
+        mBottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id == R.id.bottom_home){
+                    openHomeFragment();
+                }else if(id == R.id.bottom_book){
+                    openBookFragment();
+                }
+                return true;
+            }
+        });
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if(id == R.id.nav_home){
-            if(mCurrentFragment != FRAGMENT_HOME){
-                replaceFragment(new HomeFragment());
-                mCurrentFragment = FRAGMENT_HOME;
-            }
+            openHomeFragment();
+            mBottomNavigationView.getMenu().findItem(R.id.bottom_home).setChecked(true);
         }else if (id == R.id.nav_book){
-            if(mCurrentFragment != FRAGMENT_BOOK){
-                replaceFragment(new BookFragment());
-                mCurrentFragment = FRAGMENT_BOOK;
-            }
+            openBookFragment();
+            mBottomNavigationView.getMenu().findItem(R.id.bottom_book).setChecked(true);
         }else if (id == R.id.nav_account){
             if(mCurrentFragment != FRAGMENT_ACCOUNT){
                 replaceFragment(new AccountFragment());
@@ -66,7 +85,18 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
-
+    private void openHomeFragment(){
+        if(mCurrentFragment != FRAGMENT_HOME){
+            replaceFragment(new HomeFragment());
+            mCurrentFragment = FRAGMENT_HOME;
+        }
+    }
+    private void openBookFragment(){
+        if(mCurrentFragment != FRAGMENT_BOOK){
+            replaceFragment(new BookFragment());
+            mCurrentFragment = FRAGMENT_BOOK;
+        }
+    }
     @Override
     public void onBackPressed() {
         if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
@@ -80,5 +110,27 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.content_frame,fragment);
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_on_toolbar,menu);
+
+        MenuItem.OnActionExpandListener onActionExpandListener = new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem menuItem) {
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem menuItem) {
+                return true;
+            }
+        };
+        menu.findItem(R.id.toolbar_search).setOnActionExpandListener(onActionExpandListener);
+        SearchView searchView = (SearchView) menu.findItem(R.id.toolbar_search).getActionView();
+        searchView.setQueryHint("Tìm kiếm.....");
+        return true;
     }
 }
