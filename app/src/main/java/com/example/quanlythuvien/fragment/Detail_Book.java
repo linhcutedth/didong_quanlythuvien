22e5ea26cec135d6953254c7dc7a81bfb0f04a77
honghai19521467 <92149284+honghai19521467@users.noreply.github.com>
2022-06-06 22:47:18 +0700

    button_back

--- app/src/main/java/com/example/quanlythuvien/fragment/Detail_Book.java
+++ app/src/main/java/com/example/quanlythuvien/fragment/Detail_Book.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -11,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -27,7 +29,7 @@ import com.example.quanlythuvien.SqliteDBHelper;
 public class Detail_Book extends Fragment {
     private EditText MADAUSACH, TENDAUSACH, TACGIA, NXB,NAMXB,TONGSO,DANGCHOMUON, SANCO, VITRI,THELOAI;
     private View view;
-    private Button btnback, btnUpdate;
+    private Button btnUpdate;
     public static final String TAG = Detail_Book.class.getName();
     SqliteDBHelper db;
 
@@ -37,7 +39,7 @@ public class Detail_Book extends Fragment {
 
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_detail__book, container, false);
-        btnback = view.findViewById(R.id.back_book_btn);
+//        btnback = view.findViewById(R.id.back_book_btn);
         btnUpdate = view.findViewById(R.id.update_book_btn);
         TENDAUSACH = (EditText) view.findViewById(R.id.edit_tensach);
         TACGIA = (EditText) view.findViewById(R.id.edit_tacgia);
@@ -68,14 +70,14 @@ public class Detail_Book extends Fragment {
             }
         }
 
-        btnback.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(getFragmentManager() != null){
-                    getFragmentManager().popBackStack();
-                }
-            }
-        });
+//        btnback.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(getFragmentManager() != null){
+//                    getFragmentManager().popBackStack();
+//                }
+//            }
+//        });
 
         btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -107,4 +109,33 @@ public class Detail_Book extends Fragment {
         return view;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        MenuItem item_search = menu.findItem(R.id.toolbar_search);
+        item_search.setVisible(false);
+        MenuItem item_notice = menu.findItem(R.id.toolbar_notice);
+        item_notice.setVisible(false);
+        MenuItem item_person = menu.findItem(R.id.toolbar_person);
+        item_person.setVisible(false);
+        MenuItem item_back = menu.findItem(R.id.toolbar_back);
+        item_back.setVisible(true);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.toolbar_back){
+           if(getFragmentManager() != null){
+                getFragmentManager().popBackStack();
+           }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
