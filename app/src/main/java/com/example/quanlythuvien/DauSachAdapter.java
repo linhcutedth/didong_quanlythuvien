c563cf2342e8afa1d7016a3c16ad46935710e07a
honghai19521467 <92149284+honghai19521467@users.noreply.github.com>
2022-06-08 00:14:01 +0700

    delete

--- app/src/main/java/com/example/quanlythuvien/DauSachAdapter.java
+++ app/src/main/java/com/example/quanlythuvien/DauSachAdapter.java
@@ -1,13 +1,26 @@
 package com.example.quanlythuvien;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.quanlythuvien.fragment.BookFragment;
 
 import java.util.ArrayList;
 
@@ -16,9 +29,11 @@ public class DauSachAdapter extends RecyclerView.Adapter<DauSachAdapter.ViewHold
     private ArrayList<DauSachModels> cuonsachModalArrayList;
     private Context context;
     private IClickItemlistener iClickItemlistener;
+    private Fragment fragment;
 
     public interface IClickItemlistener{
         void onClickItemBook(DauSachModels dauSachModels);
+        void onClickDeleteBook(String maDauSach);
     }
 
     public DauSachAdapter(ArrayList<DauSachModels> cuonsachModalArrayList, Context context) {
@@ -40,6 +55,7 @@ public class DauSachAdapter extends RecyclerView.Adapter<DauSachAdapter.ViewHold
     @Override
     public void onBindViewHolder(@NonNull DauSachAdapter.ViewHolder holder, int position) {
         final DauSachModels modal = cuonsachModalArrayList.get(position);
+        holder.MA_DAUSACH.setText(Integer.toString(modal.getMA_DAUSACH()));
         holder.TENDAUSACH.setText(modal.getTENDAUSACH());
         holder.TACGIA.setText("Tác giả: " + modal.getTACGIA());
         holder.SANCO.setText("Sẵn có: " + modal.getSANCO());
@@ -63,6 +79,17 @@ public class DauSachAdapter extends RecyclerView.Adapter<DauSachAdapter.ViewHold
             TENDAUSACH = itemView.findViewById(R.id.txt_tendausach);
             TACGIA = itemView.findViewById(R.id.txt_tacgia);
             SANCO = itemView.findViewById(R.id.txt_sanco);
+            MA_DAUSACH = itemView.findViewById(R.id.txt_masach);
+
+            itemView.findViewById(R.id.trash_cuonsach).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    iClickItemlistener.onClickDeleteBook(MA_DAUSACH.getText().toString());
+                }
+            });
         }
+
     }
+
+
 }
