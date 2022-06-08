e49961041f09060a726b6f5e280029505c6c4626
linhcutedth <19521757@gm.uit.edu.vn>
2022-06-09 01:04:41 +0700

    update

--- app/src/main/java/com/example/quanlythuvien/introActivity.java
+++ app/src/main/java/com/example/quanlythuvien/introActivity.java
@@ -2,13 +2,26 @@ package com.example.quanlythuvien;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class introActivity extends AppCompatActivity {
 
+    Button bt_start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        bt_start = findViewById(R.id.bt1);
+        bt_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
